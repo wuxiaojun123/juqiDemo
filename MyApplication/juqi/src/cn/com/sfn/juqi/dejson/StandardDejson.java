f85c0abc0c94ab662917674292c49a9471f600d3
wuxiaojunIdotools <wuxiaojun@Idotools.com>
2017-08-11 18:56:07 +0800

    提交

--- MyApplication/juqi/src/cn/com/sfn/juqi/dejson/StandardDejson.java
+++ MyApplication/juqi/src/cn/com/sfn/juqi/dejson/StandardDejson.java
@@ -4,22 +4,23 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.util.Log;
+
 import cn.com.sfn.juqi.model.StandardModel;
 
 public class StandardDejson {
-	public StandardModel dejson(String str) {
-		StandardModel registerModel = new StandardModel();
-		if (str.startsWith("\ufeff")) {
-			str = str.substring(1);
-		} else {
-		}
-		try {
-			JSONObject jObject = new JSONObject(str);
-			registerModel.setStatus(jObject.getInt("status"));
-			Log.e("status",Integer.toString(jObject.getInt("status")));
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
-		return registerModel;
-	}
+    public StandardModel dejson(String str) {
+        StandardModel registerModel = new StandardModel();
+        if (str.startsWith("\ufeff")) {
+            str = str.substring(1);
+        } else {
+        }
+        try {
+            JSONObject jObject = new JSONObject(str);
+            registerModel.setStatus(jObject.getInt("status"));
+            registerModel.setInfo(jObject.getString("info"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return registerModel;
+    }
 }
